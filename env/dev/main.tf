8fc7edab41f1cb5141adebf22e8d85f73e2ec39f
Alejandra Jaime <alejandra.jaime.moros@mediamonks.com>
2024-10-15 13:36:22 -0300

    Updates dev env terraform files

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -2,7 +2,7 @@ resource "random_id" "default" {
   byte_length = 4
 }
 
-resource "google_storage_bucket" "function-bucket" {
+resource "google_storage_bucket" "default" {
   # El nombre del bucket debe ser unico a nivel global en GCP
   name                        = "${var.user-prefix}-${random_id.default.hex}-source" 
   location                    = "${var.gcp_region}"
@@ -13,47 +13,53 @@ resource "google_storage_bucket" "function-bucket" {
 data "archive_file" "default" {
   type        = "zip"
   output_path = "/tmp/function-source.zip"
-  source_dir  = "./src"
+  source_dir  = "../../src"
 }
 
-resource "google_storage_bucket_object" "cf-source" {
+resource "google_storage_bucket_object" "object" {
   name   = "${var.user-prefix}-function-src.zip"
-  bucket = google_storage_bucket.function-bucket.name
+  bucket = google_storage_bucket.default.name
   
   # Add path to the zipped function source code
   source = data.archive_file.default.output_path 
 }
 
-resource "google_cloudfunctions2_function" "default" {
+resource "google_cloudfunctions_function" "default" {
   name        = "${var.user-prefix}-airport-information-parser"
-  location    = "${var.gcp_project}"
-  description = "a new function"
+  description = "first test function"
+  runtime = "nodejs20"
 
-  build_config {
-    runtime     = "nodejs18"
+  available_memory_mb   = 256
+  source_archive_bucket = google_storage_bucket.default.name
+  source_archive_object = google_storage_bucket_object.object.name
+  trigger_http          = true
+  entry_point           = "main"
+
+  /* build_config {
+    runtime     = "nodejs20"
     entry_point = "main" # Set the entry point
     source {
       storage_source {
-        bucket = google_storage_bucket.function-bucket.name
-        object = google_storage_bucket_object.cf-source.name
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.object.name
       }
     }
-  }
+  } */
 
-  service_config {
-    max_instance_count = 1
-    available_memory   = "256M"
-    timeout_seconds    = 60
-  }
+  #service_config {
+  #  max_instance_count = 1
+  #  available_memory   = "256M"
+  #  timeout_seconds    = 60
+  #}
 }
 
 resource "google_cloud_run_service_iam_member" "member" {
-  location = google_cloudfunctions2_function.default.location
-  service  = google_cloudfunctions2_function.default.name
+  location = "${var.gcp_region}"
+  service  = google_cloudfunctions_function.default.name
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
 
 output "function_uri" {
-  value = google_cloudfunctions2_function.default.service_config[0].uri
+  value = google_cloudfunctions_function.default.https_trigger_url #default.service_config[0].uri
 }
--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = "6.5.0"
+      version = "6.7.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file(var.sa_credentials_file_path)
+  #credentials = file(var.sa_credentials_file_path)
   project = var.gcp_project
   region = var.gcp_region
 }
