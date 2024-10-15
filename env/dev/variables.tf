03edc208bd6893850864dd0ee970bfd69fd13a52
Alejandra Jaime <alejandra.jaime.moros@mediamonks.com>
2024-10-15 13:36:22 -0300

    Updates dev env terraform files

--- env/dev/variables.tf
+++ env/dev/variables.tf
@@ -4,16 +4,16 @@ variable "gcp_project" {
   default = "mm-spla-tse-internal-hub"
 }
 
-# Region en que vamos a desplegar nuestra infra
+# Region en que vamos a desplegar el bucket de GCS
 variable "gcp_region" {
   type = string
-  default = "us-east-1"
+  default = "us-east1"
 }
 
 # Ruta del archivo de credenciales 
 variable "sa_credentials_file_path" {
   type = string
-  default = "../credentials/sa-credentials.json"
+  default = "../../credentials/sa-credentials.json"
 }
 
 # Agrega un prefijo unico para identificar tu infra 
