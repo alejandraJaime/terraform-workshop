399545f2953ac0b360f65d568fca234fe5f0e03d
Alejandra Jaime <alejandra.jaime.moros@mediamonks.com>
2024-10-15 13:38:12 -0300

    Updates main src file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
-const functions = require("@google-cloud/functions-framework")
 const { BigQuery } = require("@google-cloud/bigquery")
 const { airportsByCountry } = require("./queries")
 
-functions.http("myHttpFunction", async (req, res) => {
-    async function queryTable(query) {
+const queryTable = async (query) => {
+    try {
+        /* const bqOptions = {
+            keyFilename: "path/to/service_account.json",
+            projectId: "my_project",
+        }
+        const bigquery = new BigQuery(bqOptions) */
+        const bigquery = new BigQuery()
+
         const options = {
             query: query,
             location: "US",
@@ -15,43 +21,43 @@ functions.http("myHttpFunction", async (req, res) => {
 
         console.log(`Job ${job.id} finished.`)
         return rows
+    } catch (err) {
+        console.log(err)
     }
+}
 
-    function parseAirports(rows) {
-        const operationalStatusMap = new Map([
-            ["Operational", "OP"],
-            ["Closed", "CL"],
-            ["Indefinite", "IN"],
-        ])
-        const airportUseMap = new Map([
-            ["Public", "PB"],
-            ["Private", "PR"],
-        ])
-        const airportMilCodeMap = new Map([
-            ["CIVIL", "CIV"],
-            ["OTHER", "OT"],
-        ])
-        const parsedAirports = rows.map((airport) => {
-            let operationalStatus = operationalStatusMap.get(airport.oper_status)
-            operationalStatus = operationalStatus ? operationalStatus : airport.oper_status
-            let airportUse = airportUseMap.get(airport.airport_use)
-            airportUse = airportUse ? airportUse : airport.airport_use
-            let airportMilCode = airportMilCodeMap.get(airport.mil_code)
-            airportMilCode = airportMilCode ? airportMilCode : airport.mil_code
-            return `${operationalStatusMap}|${airportUse}|${airportMilCode}|${airport.faa_identifier}-${airport.name}-${airport.service_city}`
-        })
-        return parsedAirports
-    }
+const parseAirports = (rows) => {
+    const operationalStatusMap = new Map([
+        ["Operational", "OP"],
+        ["Closed", "CL"],
+        ["Indefinite", "IN"],
+    ])
+    const airportUseMap = new Map([
+        ["Public", "PB"],
+        ["Private", "PR"],
+    ])
+    const airportMilCodeMap = new Map([
+        ["CIVIL", "CIV"],
+        ["OTHER", "OT"],
+    ])
+    const parsedAirports = rows.map((airport) => {
+        let operationalStatus = operationalStatusMap.get(airport.oper_status)
+        operationalStatus = operationalStatus ? operationalStatus : airport.oper_status
+        let airportUse = airportUseMap.get(airport.airport_use)
+        airportUse = airportUse ? airportUse : airport.airport_use
+        let airportMilCode = airportMilCodeMap.get(airport.mil_code)
+        airportMilCode = airportMilCode ? airportMilCode : airport.mil_code
+        //return `${operationalStatusMap}|${airportUse}|${airportMilCode}|${airport.faa_identifier}-${airport.name}-${airport.service_city}`
+        return `${operationalStatusMap}|${airportUse}|${airportMilCode}|${airport.faa_identifier}-${airport.name}`
+    })
+    return parsedAirports
+}
 
+const main = async (req, res) => {
     try {
         const { body } = req
-        const options = {
-            keyFilename: "path/to/service_account.json",
-            projectId: "my_project",
-        }
-        const bigquery = new BigQuery(options)
-
-        const airportQuery = airportsByCountry(body.country)
+        console.log(`Requested information for country: ${body.country}`)
+        const airportQuery = airportsByCountry(body.country, 10)
         const airports = await queryTable(airportQuery)
 
         // Parse response
@@ -60,6 +66,11 @@ functions.http("myHttpFunction", async (req, res) => {
         // Send an HTTP response
         res.status(200).send(parsedAirports)
     } catch (err) {
-        res.status(500).send("Bad Request")
+        if (res.status !== 500) {
+            res.status(400).send(`Bad Request: ${err.message}`)
+        }
+        res.status(500).send(`Server Error: ${err.message}`)
     }
-})
+}
+
+module.exports = { main }
